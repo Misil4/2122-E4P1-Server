c538945e3bad21769a1048e8c984467cd0a066b6
Misil4 <carlossilvamarques.cs@gmail.com>
2022-01-07 23:24:31 +0100

    (UPDATE) testing chat vs

--- websocket/chatSocket.js
+++ websocket/chatSocket.js
@@ -1,16 +1,16 @@
 import ChatModel from "../models/chatModel.js";
 
 export const chatSocket = (io) => {
-    io.on("connection" ,(socket) => {
+    io.sockets.on("connection" ,(socket) => {
         socket.on("chat message",msg => {
             console.log("message")
-            io.emit("chat message",msg);
+            io.sockets.emit("chat message",msg);
         })
         socket.on("insert_message",message => {
             ChatModel.create(message,(err,docs) =>{
                 if(err) return res.status(500).send({message: `Error al realizar la petición: ${err}`});
                 ChatModel.find({}).then(docs => {
-                    io.socket.emit("update_messages",docs)
+                    io.sockets.emit("update_messages",docs)
                 })
             })
             })
