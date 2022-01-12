455ce838e3b07d2bd0efc4eb8851f43271dda7fc
Ander Delgado <anderunaidel@gmail.com>
2022-01-12 09:20:27 +0100

    Hotfix on garbage socket

---
+++ websocket/garbageSocket.js
@@ -0,0 +1,42 @@
+import GarbageModel from "../models/garbageModel.js";
+
+export const garbageSocket = (io) => {
+    io.sockets.on("connection",(socket) => {
+        socket.on("garbage_data", () => {
+            GarbageModel.find({ completed: false }).then(docs => {
+                console.log("TRASH DATA")
+                console.log(docs)
+              io.sockets.emit("get_trash", docs);
+            })
+          }) 
+          socket.on("garbage_update", (id_basura) => {
+            GarbageModel.updateOne({_id: id_basura}, { $set: {completed: true} }, { new: true }, (err, docs) => {
+              if (err) return console.log("error al realizar la peticion")
+              if (!docs) return console.log("no existe el user")
+              GarbageModel.find({ completed: false }).then(docs => {
+                console.log("TRASH DATA")
+                console.log(docs)
+              io.sockets.emit("change_trash", docs);
+            })
+          })
+          })
+          socket.on("insert_garbage", (garbage) => {
+            const data = {
+              location: garbage.data,
+              message : "Recoger basura aquí",
+              completed: false,
+              user: garbage.user,
+              date: new Date(parseInt(garbage.data.timestamp))
+          };
+              GarbageModel.create({location: {latitude: data.location.latitude, longitude: data.location.longitude, timestamp:data.date}, message : data.message, completed: data.completed, user: data.user},(err,docs) =>{
+                if (err) return console.log("error al realizar la peticion")
+                if (!docs) return console.log("no existe el user")
+                  GarbageModel.find({ completed: false }).then(docs => {
+                    console.log("TRASH DATA")
+                    console.log(docs)
+                  io.sockets.emit("change_trash", docs);
+                })
+              })
+          })
+    })
+}
