5623be648d0b9548108832430514191c0c865975
aitor.palleja <aitor.palleja@ikasle.aeg.eus>
2022-01-12 10:02:40 +0100

    (UPDATE) garbageShema Updated

--- models/garbageModel.js
+++ models/garbageModel.js
@@ -3,9 +3,11 @@ import UserModel from "./userModel.js";
 const Schema = Mongoose.Schema;
 
 const GarbageSchema = Schema({
-    location : Array,
+    location : { latitude: Number, longitude: Number , timestamp: String},
     message : String,
-    user : String
+    completed : Boolean,
+    user : String,
+
 })
 const GarbageModel = Mongoose.model('garbages',GarbageSchema);
 export default GarbageModel;
