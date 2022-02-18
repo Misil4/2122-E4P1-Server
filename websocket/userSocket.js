be7baa1d612c0ad4524fb4f0043b19f8435a6da0
Misil4 <carlossilvamarques.cs@gmail.com>
2022-02-18 20:13:45 +0100

    (UPDATE) prevent to crash on qr scan

--- websocket/userSocket.js
+++ websocket/userSocket.js
@@ -1,6 +1,6 @@
 import UserModel from "../models/userModel.js"
 
-export const userSocket = (io,socket) => {
+export const userSocket = (io, socket) => {
   socket.on("user_data", (email) => {
     console.log(socket.adapter.rooms)
     UserModel.findOne({ email: email }, (err, docs) => {
@@ -11,28 +11,37 @@ export const userSocket = (io,socket) => {
       })
     })
   })
-  socket.on("qr_scanned" ,(email) => {
-    UserModel.findOne({email : email}, (err,user) => {
-      console.log("pepe")
+  socket.on("qr_scanned", (email) => {
+    try {
+      UserModel.findOne({ email: email }, (err, user) => {
+        console.log("pepe")
 
-      io.emit("scanned",true)
-    })
+        io.emit("scanned", true)
+      })
+    } catch (error) {
+      console.error(error)
+    }
   })
   socket.on("badge_update", (email) => {
-    console.log(email)
-    let login_status = true;
-    UserModel.findOne({ email: email }, (err, docs) => {
-      console.log(docs.socket)
-      if (docs.login_status) {
-        login_status = false
-      }
-      UserModel.updateOne({ email: email }, { $set: { login_status: login_status } }, { new: true }, (err, docs) => {
-        if (err) return console.log("error al realizar la peticion")
-        if (!docs) return console.log("no existe el user")
-        UserModel.find({ rol: "user" }).then(data => {
-          io.to("admin").emit("change_data", data);
+    try {
+      console.log(email)
+      let login_status = true;
+      UserModel.findOne({ email: email }, (err, docs) => {
+        console.log(docs.socket)
+        if (docs.login_status) {
+          login_status = false
+        }
+        UserModel.updateOne({ email: email }, { $set: { login_status: login_status } }, { new: true }, (err, docs) => {
+          if (err) return console.log("error al realizar la peticion")
+          if (!docs) return console.log("no existe el user")
+          UserModel.find({ rol: "user" }).then(data => {
+            io.to("admin").emit("change_data", data);
+          })
         })
       })
-    })
+    }
+    catch (error) {
+      console.error(error)
+    }
   })
 }
